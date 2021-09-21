2e509ed49f836d6f27906e4176538d86ac0e2520
serenabarish <slb181@georgetown.edu>
2021-09-21 12:03:04 -0400

    Update game.py

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 
 import random
 
-
+print("Welcome 'Player One' to my Rock, Paper, Scissors Game!")
 print("Rock, Paper, Scissors, Shoot!")
 
 # PROMPT USER FOR INPUT
@@ -11,9 +11,43 @@ user_choice = input("Choose 'rock' or 'paper' or 'scissors':")
 print("User chose:")
 print (user_choice)
 
+# VALIDATING USER INPUT
+if user_choice in ["rock", "paper", "scissors"]:
+    print("And...")
+else:
+    print ("Sorry, try playing with a different choice")
+
 # COMPUTER CHOICE (AT RANDOM)
 
 options = ["rock", "paper", "scissors"]
 computer_choice = random.choice(options)
 print("Computer chose:")
 print(computer_choice)
+
+# DETERMINING A WINNER
+
+if user_choice == "rock":
+    if computer_choice == "scissors":
+        print("Congratulations 'Player One', you won!")
+    if computer_choice == "paper":
+        print("Computer won, better luck next time!")
+    if computer_choice == "rock":
+        print("tie, play again!")
+elif user_choice == "scissors":
+    if computer_choice == "paper":
+        print("Congratulations 'Player One', you won!")
+    if computer_choice == "rock":
+        print ("Computer won, better luck next time!")
+    if computer_choice == "scissors":
+        print ("tie, play again")
+elif user_choice == "paper":
+    if computer_choice == "rock":
+        print ("Congratulations 'Player One', you won!")
+    if computer_choice == "scissors":
+        print ("Computer won, better luck next time!")
+    if computer_choice == "paper":
+        print ("tie, play again!")
+
+print("Thanks for playing!")
+
+
